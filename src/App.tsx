bd2419446b9ec08bae033d050398f121ec027fe9
Poulami Saha <saha.poulami91@gmail.com>
2024-02-11 22:07:28 +0100

    More boiler plate code added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Content from "./pagination/content";
+import Game from "./tic-tac-toe/game";
+
+function App() {
+  return (
+    <div>
+      {/* <Game /> */}
+      <Content />
+    </div>
+  );
+}
+
+export default App;
